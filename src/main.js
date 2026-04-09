52656b6ed1d65f5a8cb3ee2f4d442e521db80b2a
fira <afifaahrmdhni.com>
2026-04-09 16:54:24 +0700

    update api link

--- src/main.js
+++ src/main.js
@@ -2,9 +2,17 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios' // 1. Import axios
 import './styles/global.css'
 
-createApp(App)
-    .use(createPinia())
-    .use(router)
-    .mount('#app')
+// 2. Set alamat Backend Ngrok kamu di sini
+axios.defaults.baseURL = 'https://floppiest-cordia-pharmaceutically.ngrok-free.dev/api'
+
+const app = createApp(App)
+
+// 3. Pasang axios ke global properties supaya bisa dipake di semua komponen
+app.config.globalProperties.$axios = axios
+
+app.use(createPinia())
+app.use(router)
+app.mount('#app')
